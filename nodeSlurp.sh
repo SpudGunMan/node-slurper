415f4a1b88bf11f9a2a2659f617d9486f7c3733a
SpudGunMan <kellykeeton@yahoo.com>
2024-10-03 12:09:49 -0700

    🔂looper
    enhanced with loops and functions for more nodes

--- nodeSlurp.sh
+++ nodeSlurp.sh
@@ -3,31 +3,165 @@
 # 2024 Kelly Keeton K7MHI - MIT License
 # Tool uses meshtastic CLI methods to slurp node data from a meshtastic device
 
-cwd=$(pwd)
-echo "                              "
-echo "    o   o        o            "
-echo "    |\  |        |            "
-echo "    | \ | o-o  o-O o-o        "
-echo "    |  \| | | |  | |-'        "
-echo "    o   o o-o  o-o o-o        "
-echo "                              "
-echo " o-o  o                       "
-echo "|     |                       "
-echo " o-o  | o  o o-o o-o  o-o o-o "
-echo "    | | |  | |   |  | |-' |   "
-echo "o--o  o o--o o   O-o  o-o o   "
-echo "                 |            "
-echo "                 o            "
-echo "                              "
-echo " slurping node...             "
-echo "                              "
+# User defined variables
+injectKeys=true
 
-# look for meshtastic has a bin in the path
-if whereis meshtastic | grep -q bin/meshtastic; then
+function injectData() {
+    # inject commands into the node update the following commands with your data
+    # uncomment and add your keys to the end of line
+    echo ""
+    echo "attempting to inject commands into the node, please wait..."
+    #meshtastic --ch-add MeshAround --ch-set psk base64:
+    #sleep 1
+    #meshtastic --set security.admin_key base64:
+    #sleep 1
+    #meshtastic --ch-add admin --ch-set psk base64:
+    #sleep 1
+    #meshtastic --set lora.region US
+    #echo "done, happy meshing!"
+    echo "no commands to inject"
+    echo ""
+    echo "waiting for new device or press ctrl-c to exit"
+}
+
+# end of user defined variables
+
+cwd=$(pwd)
+
+function slurpData() {
+    # no device detected
+    if grep -q "No Serial Meshtastic device detected" $cwd/node-info.txt; then
+        echo "plug a device in via USB"
+    else
+        # parse the node-info.txt for node number
+        nodeNum=$(grep -o '"myNodeNum": [0-9]*' $cwd/node-info.txt| grep -o '[0-9]*')
+
+        #move the node-info.txt to nodeNum-Info.txt
+        mv $cwd/node-info.txt $cwd/$nodeNum-Info.txt
+
+        #move the node.yaml to nodeNum.yaml
+        mv $cwd/node.yaml $cwd/$nodeNum.yaml
+
+        # channel keys string
+        channelKeys=$(grep -o '"psk": ".*", "name": ".*"' $cwd/$nodeNum-Info.txt | grep -o '"psk": ".*"')
+
+        # parse the owner from the node.yaml
+        owner=$(grep -o 'owner: ".*"' $cwd/$nodeNum.yaml | grep -o '".*"')
+
+        # MQTT password 
+        mqttPass=$(grep -o 'password: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
+
+        # bluetooth pin "fixedPin: 123456" the pin is 123456
+        fixedPin=$(grep -o 'fixedPin: [0-9]*' $cwd/$nodeNum.yaml | grep -o '[0-9]*')
+
+        # wifi password
+        wifiPsk=$(grep -o 'wifiPsk: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
+
+        # admin key string
+        adminKey=$(grep -o ' \-.*' $cwd/$nodeNum.yaml | grep -o ' .*')
+
+        # dm private key
+        dmPrivateKey=$(grep -o 'privateKey: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
+
+        # dm public key
+        dmPublicKey=$(grep -o 'publicKey: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
+
+        # display the important node data to stdout
+        echo "node data slurped for node $nodeNum named $owner"
+        echo "channel keys:" 
+        echo "$channelKeys"
+        echo "fixed pin:$fixedPin"
+        echo "wifi password:$wifiPsk"
+        echo "mqtt password:$mqttPass"
+        echo "admin key:$adminKey"
+        echo "dm private key:$dmPrivateKey"
+        echo "dm public key:$dmPublicKey"
+        echo "node data saved to $nodeNum-Info.txt and $nodeNum.yaml"
+        echo ""
+        if [ "$injectKeys" = false ]; then
+            echo "waiting for new device or press ctrl-c to exit"
+        fi
+    fi
+}
+
+function saveData() {
     # collect node-info.txt
     meshtastic --info > $cwd/node-info.txt
     # collect node.yaml
     meshtastic --export-config > $cwd/node.yaml
+
+    # no device detected
+    if grep -q "No Serial Meshtastic device detected" $cwd/node-info.txt; then
+        echo "No device detected"
+    else
+        # parse the node-info.txt for node number
+        nodeNum=$(grep -o '"myNodeNum": [0-9]*' $cwd/node-info.txt | grep -o '[0-9]*')
+        echo "node $nodeNum detected"
+    fi
+}
+
+# start of main script
+
+echo "                                "
+echo "    o   o        o              "
+echo "    |\  |        |              "
+echo "    | \ | o-o  o-O o-o          "
+echo "    |  \| | | |  | |-'          "
+echo "    o   o o-o  o-o o-o          "
+echo "                                "
+echo " o-o  o                         "
+echo "|     |                         "
+echo " o-o  | o  o o-o o-o  o-o o-o   "
+echo "    | | |  | |   |  | |-' |     "
+echo "o--o  o o--o o   O-o  o-o o     "
+echo "                 |              "
+echo "                 o              "
+echo "                                "
+echo " waiting for node to connect    "
+echo "                                "
+echo "                    spudgunman24"
+echo "                                "
+
+# look for meshtastic has a bin in the path
+if whereis meshtastic | grep -q bin/meshtastic; then
+
+    # Assume a device is connected already
+    echo " trying to slurp, could take a moment..."
+    # collect node-info.txt and node.yaml
+    saveData
+
+    # slurp the data
+    slurpData
+
+    if [ "$injectKeys" = true ]; then
+        injectData
+    fi
+
+    # detect new device loop for any subsequent devices
+    while true; do
+        
+        # detect new USB serial device
+        ls /dev/tty* > $cwd/.nodeSlurp.before
+        sleep 2
+        ls /dev/tty* > $cwd/.nodeSlurp.after
+
+        if diff $cwd/.nodeSlurp.before $cwd/.nodeSlurp.after; then
+            sleep 1
+        else
+            echo "new device detected trying to slurp, could take a moment..."
+            echo ""
+
+            # collect node-info.txt and node.yaml
+            saveData
+
+            # slurp the data
+            slurpData
+
+            if [ "$injectKeys" = true ]; then
+                injectData
+            fi
+        fi
+    done
 
     # evaluate the node-info.txt for meshtastic version
     if grep -q "pip install --upgrade meshtastic" $cwd/node-info.txt; then
@@ -35,78 +169,13 @@ if whereis meshtastic | grep -q bin/meshtastic; then
         echo "pip install --upgrade meshtastic"
         echo "pip install --upgrade meshtastic --break-system-packages"
     fi
-    # exit check
-    if grep -q "No Serial Meshtastic device detected" $cwd/node-info.txt; then
-        echo "No Meshtastic device found exiting"
-        exit 1
-    fi
 else
-    # install meshtastic if it is not found
+    # if it is not found
     echo "meshtastic not found"
-    sudo apt-get install -y python3-pip
-    pip3 install -U meshtastic
-fi
-
-# parse the node-info.txt for node number
-nodeNum=$(grep -o '"myNodeNum": [0-9]*' $cwd/node-info.txt | grep -o '[0-9]*')
-
-#move the node-info.txt to nodeNum-Info.txt
-mv $cwd/node-info.txt $cwd/$nodeNum-Info.txt
-
-# channel keys string
-channelKeys=$(grep -o '"psk": ".*", "name": ".*"' $cwd/$nodeNum-Info.txt | grep -o '"psk": ".*"')
-
-#move the node.yaml to nodeNum.yaml
-mv $cwd/node.yaml $cwd/$nodeNum.yaml
-
-# parse the owner from the node.yaml
-owner=$(grep -o 'owner: ".*"' $cwd/$nodeNum.yaml | grep -o '".*"')
-
-# MQTT password 
-mqttPass=$(grep -o 'password: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
-
-# bluetooth pin "fixedPin: 123456" the pin is 123456
-fixedPin=$(grep -o 'fixedPin: [0-9]*' $cwd/$nodeNum.yaml | grep -o '[0-9]*')
-
-# wifi password
-wifiPsk=$(grep -o 'wifiPsk: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
-
-# admin key string
-adminKey=$(grep -o ' \-.*' $cwd/$nodeNum.yaml | grep -o ' .*')
-
-# dm private key
-dmPrivateKey=$(grep -o 'privateKey: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
-
-# dm public key
-dmPublicKey=$(grep -o 'publicKey: .*' $cwd/$nodeNum.yaml | grep -o ' .*')
-
-echo "node data slurped for node $nodeNum named $owner"
-echo "channel keys:" 
-echo "$channelKeys"
-echo "fixed pin:$fixedPin"
-echo "wifi password:$wifiPsk"
-echo "mqtt password:$mqttPass"
-echo "admin key:$adminKey"
-echo "dm private key:$dmPrivateKey"
-echo "dm public key:$dmPublicKey"
-echo "node data saved to $nodeNum-Info.txt and $nodeNum.yaml"
-echo ""
-
-# ask if user wants to inject any keys into the node
-echo "Do you want to inject any data into the node?"
-read -p "y/n: " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    echo ""
-    echo "exiting, happy meshing!"
+    echo "apt-get install -y python3-pip"
+    echo "install -U meshtastic"
     exit 1
 fi
 
-echo "attempting to inject commands into the node, please wait..."
-#meshtastic --ch-add MeshAround --ch-set psk base64:
-#meshtastic --set security.admin_key base64:
-#meshtastic --ch-add admin --ch-set psk base64:
-echo "no commands to inject"
-#meshtastic --set lora.region US
-echo "done, happy meshing!"
 exit 0
 # end of script
